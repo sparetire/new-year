04de9bf36e1ba5048f3c2318faf5a49368a208e8
Sparetire <s9ar371r3@gmail.com>
2017-01-23 23:57:07 +0800

    page-6 100%

--- src/config/routes.base.js
+++ src/config/routes.base.js
@@ -4,6 +4,7 @@ const page2 = resolve => require(['../components/page-2'], resolve);
 const page3 = resolve => require(['../components/page-3'], resolve);
 const page4 = resolve => require(['../components/page-4'], resolve);
 const page5 = resolve => require(['../components/page-5'], resolve);
+const page6 = resolve => require(['../components/page-6'], resolve);
 
 const routes = [{
 	name: 'page0',
@@ -29,6 +30,10 @@ const routes = [{
 	name: 'page5',
 	path: '/page-5',
 	component: page5
+}, {
+	name: 'page6',
+	path: '/page-6',
+	component: page6
 }, {
 	path: '*',
 	redirect: '/page-0'
